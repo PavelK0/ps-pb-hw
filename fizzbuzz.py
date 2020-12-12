8a5e03bcd476faa3f671207aebe60da941d95d28
Pavel <63403198+PavelK0@users.noreply.github.com>
2020-12-13 00:20:48 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,10 +2,11 @@ import os
 
 os.system('clear')
 
-count = 0
+count, summ = 0, 0
 
 for i in range(1000,20001):
     if i % 3 == 0 and i % 5 == 0:
         count += 1
+        summ += i
 
-print(f'В диапазоне [1000,20000] всего {count} чисел делятся и на 3, и на 5.')
+print(f'В диапазоне [1000,20000] всего {count} чисел делятся и на 3, и на 5. Их сумма - {summ}.')
